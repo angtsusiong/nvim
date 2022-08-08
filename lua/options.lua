298fefecfd2913d8eebea8703add86feed322201
a9g <angtsusiong@gmail.com>
2022-08-08 08:14:24 +0800

    Config color scheme

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,18 @@
 local options = {
+	completeopt = { 'menuone', 'noselect' },
+	signcolumn = 'yes',
+	ignorecase = true,
+  showmode = false,
+  showtabline = 2,
+  smartindent = true,
+  tabstop = 2,
+  pumheight = 10,
+  cmdheight = 2,
+  termguicolors = true,
   cursorline = true,
   number = true,
   relativenumber = true,
+	wrap = false,
   numberwidth = 4,
   shiftwidth = 2,
 }
